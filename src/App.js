a99829afaa3d8a51f7aac1d22a49dd2336afd9eb
mridulmanocha <mridul.manocha22@gmail.com>
2020-12-31 00:43:18 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>I am a h1 tag</h1>
+        <Person/>
+      </div>
+    );
+  }
+} 
+
+export default App;
